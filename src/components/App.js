8799654e74a733cee696b9e256bd35ee58ceb8e3
Thane Engen <thaneengen@gmail.com>
2019-09-23 16:51:04 -0500

    Add and Implement SearchBar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Wikipedia from './Wikipedia';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
     render() {
-        return <div><Wikipedia /></div>
+        return <div><SearchBar /></div>;
     }
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onSubmitForm = event => {
+    event.preventDefault();
+/////////////////////////////////////////////////
+    console.log(this.state.term)
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm}>
+        <div className="ui huge icon input">
+          <input type="text" placeholder="Search" value={this.state.term} onChange={e => this.setState({ term: e.target.value })}/>
+          <i className="search icon"></i>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
