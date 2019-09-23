755defaaffcfb13128760f4a58fb2e2ee420fcaa
Thane Engen <thaneengen@gmail.com>
2019-09-23 15:19:54 -0500

    Setup index.js, App and Wikipedia components

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Wikipedia from './Wikipedia';
+
+class App extends React.Component {
+    render() {
+        return <div><Wikipedia /></div>
+    }
+}
+
+export default App;
---
+++ src/components/Wikipedia.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './App';
+
+class Wikipedia extends React.Component {
+    render() {
+        return <div>WIKIPEDIA COMPONENT</div>
+    }
+}
+
+export default Wikipedia;
