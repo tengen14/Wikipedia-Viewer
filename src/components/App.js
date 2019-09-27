264a2205b3549f4ee99c9fef9b9a38d3873a2891
Thane Engen <thaneengen@gmail.com>
2019-09-27 16:41:20 -0500

    match search term with wikipedia search request

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,40 @@
+import _ from "lodash";
 import React from "react";
 import SearchBar from "./SearchBar";
 //import fetchData from "../apis/wikipediaAPI";
 
 class App extends React.Component {
-  state = { titles: [], snippets: [] };
+  state = { titles: null, snippets: null };
 
-  onSearchSubmit = state => {
+  onSearchSubmit = term => {
     const fetchPromise = fetch(
-      "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=dogs&srlimit=20&format=json&origin=*"
+      `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${term}&srlimit=20&format=json&origin=*`
     );
 
     fetchPromise
       .then(response => {
-        console.log(response);
         return response.json();
       })
       .then(data => {
         console.log(data);
+
         const searchData = data.query.search;
 
+        //function that sorts api data into 2 separate arrays of data, 'titles' and 'snippets'
         const titles = [];
-        for (let data of searchData) {
-          titles.push(data.title);
-          this.setState({ titles: titles });
-        }
+        const snippets = [];
+        for (let entry of searchData) {
+          console.log(entry);
+          const title = _.pick(entry, ["title"]).title;
+          const snippet = _.pick(entry, ["snippet"]).snippet;
 
-        console.log("state", this.state.titles);
+          titles.push(title);
+          snippets.push(snippet);
+        }
+        this.setState({ titles: titles, snippets: snippets });
+
+        console.log("state", this.state.titles, this.state.snippets);
+        console.log(term);
       });
   };
 
@@ -33,9 +42,8 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ol>
-          <li>{this.state.titles}</li>
-        </ol>
+        <h1>{this.state.titles}</h1>
+        <p>{this.state.snippets}</p>
       </div>
     );
   }
