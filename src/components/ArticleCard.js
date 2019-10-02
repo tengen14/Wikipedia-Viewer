cda851940adf806f25746045e2873f354c3882e2
Thane Engen <thaneengen@gmail.com>
2019-10-02 15:59:03 -0500

    Fix bug that allows user to click link outside article divs

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -4,7 +4,10 @@ import "../styles/ArticleCard.css";
 const ArticleCard = props => {
   return (
     <div>
-      <a href={`https://en.wikipedia.org/?curid=${props.pageid}`} target="_blank">
+      <a
+        href={`https://en.wikipedia.org/?curid=${props.pageid}`}
+        target="_blank"
+      >
         <div className="ui segment">
           <h2 className="ui left floated header">{props.title}</h2>
           <div className="ui clearing divider"></div>
